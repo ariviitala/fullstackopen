5f6c04d572d5f9a8a22f6e335a439097182bd4c2
Ari Viitala <amt.viitala@gmail.com>
2020-04-28 11:52:51 +0300

    Added feature that fetches existing phonebook from json-server

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = (props) => {
     return (
@@ -10,9 +11,9 @@ const Filter = (props) => {
 
     return (
         <div>
-          <form onSubmit={(event) => event.preventDefault()}>
-            Filter shown with: <input value={props.value} onChange={props.changeHandler}/>
-          </form>
+            <form onSubmit={(event) => event.preventDefault()}>
+                Filter shown with: <input value={props.value} onChange={props.changeHandler} />
+            </form>
         </div>
     )
 }
@@ -21,70 +22,72 @@ const AddContact = (props) => {
 
     return (
         <form onSubmit={props.submitHandler}>
-          <div>
-            name: <input value={props.nameValue} onChange={props.nameHandler}/>
-          </div>
-            number: <input value={props.numberValue} onChange={props.numberHandler}/>
-          <div>
-            <button type="submit">add</button>
-          </div>
-      </form>
+            <div>
+                name: <input value={props.nameValue} onChange={props.nameHandler} />
+            </div>
+            number: <input value={props.numberValue} onChange={props.numberHandler} />
+            <div>
+                <button type="submit">add</button>
+            </div>
+        </form>
     )
 }
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-      ])
+    const [persons, setPersons] = useState([])
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
+    const [filterString, setFilterString] = useState('')
 
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber] = useState('')
-  const [ filterString, setFilterString] = useState('')
-  
+    const hook = () => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons(response.data)
+            })
+    }
 
-  const addPerson = (event) => {
-      event.preventDefault()
-      if (persons.filter(p => p.name === newName).length > 0){
-        alert(`${newName} is already added to phonebook.`)
-      } else {
-        const newPerson = {name: newName, number: newNumber}
-        setPersons(persons.concat(newPerson))
-        setNewName('')
-        setNewNumber('')   
-      }
-      
-  }
+    useEffect(hook, [])
 
-  const handlePersonChange = (event) => {
-      setNewName(event.target.value)
-  }
+    const addPerson = (event) => {
+        event.preventDefault()
+        if (persons.filter(p => p.name === newName).length > 0) {
+            alert(`${newName} is already added to phonebook.`)
+        } else {
+            const newPerson = { name: newName, number: newNumber }
+            setPersons(persons.concat(newPerson))
+            setNewName('')
+            setNewNumber('')
+        }
+    }
 
-  const handleNumberChange = (event) => {
-      setNewNumber(event.target.value)
-  }
+    const handlePersonChange = (event) => {
+        setNewName(event.target.value)
+    }
 
-  const filterPersons = (event) => {
-    event.preventDefault()
-    setFilterString(event.target.value)
-  }
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
+    }
 
-  const personsToShow = persons.filter(p => p.name.toLowerCase().search(filterString.toLowerCase()) !== -1)
+    const filterPersons = (event) => {
+        event.preventDefault()
+        setFilterString(event.target.value)
+    }
 
-  return (
-    <div>
-      <h1>Phonebook</h1>
-      <h2>Find</h2>
-        <Filter value={filterString} changeHandler={filterPersons}/>
-      <h2>Add New</h2>
-        <AddContact nameValue={newName} numberValue={newNumber} nameHandler={handlePersonChange} 
-                    numberHandler={handleNumberChange} submitHandler={addPerson}/>
-      <h2>Numbers</h2>
-        {personsToShow.map(p =><Person key={p.name} name={p.name} number={p.number}/>)}
-    </div>
-  )
+    const personsToShow = persons.filter(p => p.name.toLowerCase().search(filterString.toLowerCase()) !== -1)
+
+    return (
+        <div>
+            <h1>Phonebook</h1>
+            <h2>Find</h2>
+            <Filter value={filterString} changeHandler={filterPersons} />
+            <h2>Add New</h2>
+            <AddContact nameValue={newName} numberValue={newNumber} nameHandler={handlePersonChange}
+                numberHandler={handleNumberChange} submitHandler={addPerson} />
+            <h2>Numbers</h2>
+            {personsToShow.map(p => <Person key={p.name} name={p.name} number={p.number} />)}
+        </div>
+    )
 
 }
 
